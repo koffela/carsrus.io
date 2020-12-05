25f786c64c86c5cb04c3697adff6e05f88f3ac17
Aaron Koffel <amkoffel@gmail.com>
2020-12-05 14:32:33 -0800

    added back the credentials file

--- step0/webapp.py
+++ step0/webapp.py
@@ -5,21 +5,11 @@ from db_connector.db_connector import connect_to_database, execute_query
 webapp = Flask(__name__)
 
 #provide a route where requests on the web application can be addressed
-@webapp.route('/hello')
+#@webapp.route('/hello')
 #provide a view (fancy name for a function) which responds to any requests on this route
-def hello():
-    return "Hello World!";
+#def hello():
+#    return "Hello World!";
 
 @webapp.route('/')
 def index():
-    print("Running queries for project step 0")
-    db_connection = connect_to_database()
-    drop_table = "DROP TABLE IF EXISTS diagnostic;"
-    create_table = "CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL);"
-    insert_row = "INSERT INTO diagnostic (text) VALUES ('MySQL is Working!')"
-    query = "SELECT * FROM diagnostic;"
-    execute_query(db_connection, drop_table);
-    execute_query(db_connection, create_table);
-    execute_query(db_connection, insert_row);
-    values = execute_query.fetchall(db_connection, query);
-    return render_template('home.html', results=values)
+    return render_template('index.html')
