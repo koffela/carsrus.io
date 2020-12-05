eca52a27fed2fd10c71a8beaab9e40bab2aec5ff
Aaron Koffel <amkoffel@gmail.com>
2020-12-05 14:40:26 -0800

    removed cred file from gitignore and added back

---
+++ db_credentials.py
@@ -0,0 +1,11 @@
+  
+# To actually have your app use this file, you need to RENAME the file to db_credentials.py
+# You will find details about your CS340 database credentials on Canvas.
+
+# the following will be used by the db_connector.py and 
+# in turn by all the Python code in this codebase to interact with the database
+
+host = 'carsrusdb.cflvftqirmoz.us-east-1.rds.amazonaws.com'  #DON'T CHANGE ME UNLESS THE INSTRUCTIONS SAY SO
+user = 'masteruser' #CHANGE ME
+passwd = 'carsrusdb' #CHANGE ME
+db = 'carsrusdb' #CHANGE ME
