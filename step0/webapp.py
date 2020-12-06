80fda12f8b3f22e8eda30fd9a71dcca044c6cf0c
Aaron Koffel <amkoffel@gmail.com>
2020-12-06 11:36:37 -0800

    added back the updated HTML files. Added insert/select functionality for cart, orders, products_orders. HTML needs adjustment.

--- step0/webapp.py
+++ step0/webapp.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask import request, redirect, render_template,
+from flask import request, redirect
 from db_connector.db_connector import connect_to_database, execute_query
 #create the web application
 webapp = Flask(__name__)
@@ -10,24 +10,74 @@ webapp = Flask(__name__)
 #def hello():
 #    return "Hello World!";
 
-@webapp.route('/')
-def index():
-    return render_template('index.html')
+@webapp.route('/browse_cart')
+#the name of this function is just a cosmetic thing. this is a sample query and I'm unsure if it's needed
+def browse_cart():
+    print("Fetching and rendering page")
+    db_connection = connect_to_database()
+    query = "SELECT * FROM cart;"
+    result = execute_query(db_connection, query).fetchall()
+    print(result)
+    return render_template('cart.html', rows=result)
 
-@webapp.route('/cart')
-def cart():
-    return render_template('cart.html')
+@webapp.route('/insert_cart', methods=['POST','GET'])
+def insert_cart():
+    db_connection = connect_to_database()
+    if request.method == 'GET':
+        query = 'SELECT * FROM cart'
+        result = execute_query(db_connection, query).fetchall()
+        print(result)
 
-@webapp.route('/insert', methods = ['POST'])
-def insert():
-    if request.method == 'POST':
+        return render_template('cart.html', carts = result)
+    elif request.method == 'POST':
+        print("Add new cart!")
         cartID = request.form['cartID']
         productID = request.form['productID']
         totalCost = request.form['totalCost']
         quantity = request.form['quantity']
-        db.insert_vehicle(cartID, productID, totalCost, quantity)
-        vehicles = db.get_vehicles()
-        print(vehicles)
-        for vehicle in vehicles:
-            var = vehicle
-        return render_template('cart.html', var = var)
+
+        query = "INSERT INTO cart (cartID, pid, totalCost, quantity) VALUES (%d, %d, %d, %d)"
+        data = (cartID, (SELECT pid FROM products WHERE pid = productID), totalCost, quantity))
+        execute_query(db_connection, query, data)
+        return ('Cart added!')
+
+#cart has insert and select done. inserting into cart requires a valid pid from products table
+
+@webapp.route('/insert_order', methods=['POST','GET'])
+def insert_order():
+    db_connection = connect_to_database()
+    if request.method == 'GET':
+        query = 'SELECT * FROM orders'
+        result = execute_query(db_connection, query).fetchall()
+        print(result)
+
+        return render_template('orders.html', orders = result)
+    elif request.method == 'POST':
+        print("Add new order!")
+        orderNumber = request.form['orderNumber']
+        cartID = request.form['cartID']
+        finalPrice = request.form['finalPrice']
+        query = "INSERT INTO orders (orderNumber, cartID, finalPrice) VALUES (%d, %d, %d)"
+        data = (orderNumber, (SELECT cid FROM cart WHERE cid = cartID), totalCost, quantity))
+        execute_query(db_connection, query, data)
+        return ('Order added!')
+
+#orders has insert and select done
+
+@webapp.route('/insert_po', methods=['POST','GET'])
+def insert_po():
+    db_connection = connect_to_database()
+    if request.method == 'GET':
+        query = 'SELECT * FROM products_orders'
+        result = execute_query(db_connection, query).fetchall()
+        print(result)
+
+        return render_template('products_orders.html', orders = result)
+    elif request.method == 'POST':
+        print("Add new product_order!")
+        productID = request.form['productID']
+        orderNumber = request.form['orderNumber']
+        query = "INSERT INTO products_orders (productID, orderNumber) VALUES (%d, %d)"
+        data = ((SELECT pid FROM products WHERE pid = productID), (SELECT orderNumber FROM orders WHERE orderNumber = orderNumber))
+        execute_query(db_connection, query, data)
+        return ('Products_order added!')
